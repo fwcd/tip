294dc96209a15977f2150e669c67ec516983e2d4
fwcd <fwcdmail@gmail.com>
2020-07-08 23:11:35 +0200

    Remove unnecessary datatype constraint

--- src/Tip/Frontend/AST/Expr.hs
+++ src/Tip/Frontend/AST/Expr.hs
@@ -7,16 +7,16 @@ import Tip.Frontend.AST.Pretty
 import Tip.Frontend.AST.VarName
 
 -- An expression AST node.
-data Show a => Expr a = LitStr a String                 -- "abc"
-                      | LitInt a Int                    -- 123
-                      | Var a String                    -- abc
-                      | Apply a (Expr a) (Expr a)       -- x y
-                      | Lambda a VarName (Expr a)      -- \x -> y
-                      | Let a VarName (Expr a) (Expr a) -- let v = x in y
+data Expr a = LitStr a String                 -- "abc"
+            | LitInt a Int                    -- 123
+            | Var a String                    -- abc
+            | Apply a (Expr a) (Expr a)       -- x y
+            | Lambda a VarName (Expr a)      -- \x -> y
+            | Let a VarName (Expr a) (Expr a) -- let v = x in y
     deriving (Show, Eq)
 
 -- Extracts the value from the expression.
-value :: Show a => Expr a -> a
+value :: Expr a -> a
 value e = case e of
     LitStr x _   -> x
     LitInt x _   -> x
@@ -25,7 +25,7 @@ value e = case e of
     Lambda x _ _ -> x
     Let x _ _ _  -> x
 
-instance (Pretty a, Show a) => Pretty (Expr a) where
+instance Pretty a => Pretty (Expr a) where
     pretty e = case e of
         LitStr t s -> "\"" <> s <> "\" :: " <> pretty t
         LitInt t i -> show i <> " :: " <> pretty t
