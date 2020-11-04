7b283d878e16ce880356c36a0d00068fc47e8054
fwcd <fwcdmail@gmail.com>
2020-11-04 02:27:45 +0100

    Smarter type annotation printing

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import Data.Either.Combinators (fromRight')
 import qualified Data.Text.IO as TIO
 import Prettyprinter.Render.Text (hPutDoc, putDoc)
 import System.Environment
 import System.IO (withFile, IOMode (..))
 import Tip.Frontend.AbstractHaskell.Parse (parseExpr)
 import Tip.Frontend.Check.TypeCheck (typeCheck)
+import Tip.Utils.General
 import Tip.Utils.Pretty
 
 main :: IO ()
@@ -15,7 +15,7 @@ main = do
     case args of
         (inFile:outFile:_) -> do
             source <- TIO.readFile inFile
-            let ast = fromRight' $ parseExpr inFile source
+            let ast = fromRight $ parseExpr inFile source
                 typedAST = typeCheck ast
 
             putDoc $ prettyPrec 0 ast
--- src/Tip/Frontend/AbstractHaskell/Expr.hs
+++ src/Tip/Frontend/AbstractHaskell/Expr.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Tip.Frontend.AbstractHaskell.Expr
     ( Expr (..)
     , exprType
@@ -13,25 +13,42 @@ data Expr a = LitStr a T.Text                 -- "abc"
             | LitInt a Int                    -- 123
             | Var a T.Text                    -- abc
             | Apply a (Expr a) (Expr a)       -- x y
-            | Lambda a VarName (Expr a)      -- \x -> y
+            | Lambda a VarName (Expr a)       -- \x -> y
             | Let a VarName (Expr a) (Expr a) -- let v = x in y
+            | Typed a (Expr a)                -- x :: t
     deriving (Show, Eq)
 
 -- Extracts the type from the expression.
 exprType :: Expr a -> a
 exprType e = case e of
-    LitStr x _   -> x
-    LitInt x _   -> x
-    Var x _      -> x
-    Apply x _ _  -> x
-    Lambda x _ _ -> x
-    Let x _ _ _  -> x
+    LitStr t _   -> t
+    LitInt t _   -> t
+    Var t _      -> t
+    Apply t  _ _ -> t
+    Lambda t _ _ -> t
+    Let t _ _ _  -> t
+    Typed t _    -> t
 
-instance PrettyPrec a => PrettyPrec (Expr a) where
-    prettyPrec p e = case e of
-        LitStr t s    -> "\"" <> prettyPrec 0 s <> "\"" <+> "::" <+> prettyPrec 0 t
-        LitInt t i    -> prettyPrec 0 i <+> "::" <+> prettyPrec 0 t
-        Var t v       -> prettyPrec 0 v <+> "::" <+> prettyPrec 0 t
-        Apply t f x   -> parensIf (p > 1) $ prettyPrec 2 f <+> prettyPrec 1 x <+> "::" <+> prettyPrec 0 t
-        Lambda t x e' -> "\\" <> prettyPrec 0 x <+> "->" <+> prettyPrec 0 e' <+> "::" <+> prettyPrec 0 t
-        Let t x e' b  -> vcat ["let" <+> prettyPrec 0 x <+> "=" <+> prettyPrec 0 e', "in" <+> prettyPrec 0 b <+> "::" <+> prettyPrec 0 t]
+-- Inserts explicit type annotation nodes at the
+-- top-most level and at every let binding.
+typeAnnotated :: Expr a -> Expr a
+typeAnnotated e = Typed (exprType e) $ typeAnnotated' e
+    where typeAnnotated' e = case e of
+            Typed t x     -> typeAnnotated' x
+            Apply t f x   -> Apply t (typeAnnotated' f) (typeAnnotated' x)
+            Lambda t x e' -> Lambda t x $ typeAnnotated' e'
+            Let t x e' b  -> Let t x (typeAnnotated e') (typeAnnotated b)
+            _             -> e
+
+instance (Show a, PrettyPrec a) => PrettyPrec (Expr a) where
+    prettyPrec p = prettyPrec' p . typeAnnotated
+        where prettyPrec' :: Int -> Expr a -> Doc ann
+              prettyPrec' p e = case e of
+                -- TODO: Figure out the correct precedences here
+                Var _ v       -> parensIf (p > 3) $ pretty v
+                LitStr _ s    -> parensIf (p > 3) $ "\"" <> pretty s <> "\""
+                LitInt _ i    -> parensIf (p > 3) $ pretty i
+                Apply _ f x   -> parensIf (p > 1) $ prettyPrec' 1 f <+> prettyPrec' 2 x
+                Lambda _ x e' -> parensIf (p > 0) $ ("\\" <> pretty x <+> "->" <+> prettyPrec' 0 e')
+                Let _ x e' b  -> parensIf (p > 0) $ vcat ["let" <+> pretty x <+> "=" <+> prettyPrec' 0 e', "in" <+> prettyPrec' 1 b]
+                Typed t x     -> parensIf (p > 0) $ prettyPrec' 1 x <+> "::" <+> prettyPrec 0 t
